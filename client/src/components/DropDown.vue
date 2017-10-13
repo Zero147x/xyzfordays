3005faa94911670030f41e318fbe00ca57c9c9be
Matthew Bomas <zero147x@gmail.com>
2017-10-13 21:59:15 +0000

    added method for admins to kick users

--- client/src/components/DropDown.vue
+++ client/src/components/DropDown.vue
@@ -1,9 +1,9 @@
 <template>
   <b-dropdown :text="user" variant="link">
-    <b-dropdown-item v-if="this.$store.getters.admin">
+    <b-dropdown-item v-if="this.$store.getters.admin" @click.stop="kick">
       kick
     </b-dropdown-item>
-    <b-dropdown-item v-if="this.$store.getters.admin">
+    <b-dropdown-item v-if="this.$store.getters.admin" @click.stop="ban">
       ban
     </b-dropdown-item>
   </b-dropdown>
@@ -11,5 +11,13 @@
 <script>
   export default {
     props: ['user'],
+    methods: {
+      kick () {
+        this.$socket.emit('kick', this.user)
+      },
+      ban () {
+        this.$socket.emit('ban', this.user)
+      }
+    }
   }
 </script>
